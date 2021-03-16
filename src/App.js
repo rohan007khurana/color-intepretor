e56d6bede21803a6dfe354efef6515a677469f92
Rohan Khurana <50539462+rohan007khurana@users.noreply.github.com>
2021-03-17 00:21:28 +0530

    Color Intepreter Choose the color from below or enter the color by copy pasting to get the symbolization of the color

--- src/App.js
+++ src/App.js
@@ -1,9 +1,66 @@
 import "./styles.css";
+import React, { useState } from "react";
+
+const colorDictionary = {
+  "🟠": "joy & warmth",
+  "🟣": "royalty & luxury",
+  "⚫": "power & mystery",
+  "🟡": "happiness & optimism",
+  "🔵": "cool & relaxing",
+  "🟢": "tranquility & good luck",
+  "⚪": "purity & innocence",
+  "🔴": "violence & danger"
+};
+
+var colorsWeKnow = Object.keys(colorDictionary);
 
 export default function App() {
+  const [meaning, setMeaning] = useState("");
+  function inputColorHandler(event) {
+    var userInput = event.target.value;
+
+    var meaning = colorDictionary[userInput];
+
+    console.log(meaning);
+
+    if (meaning === undefined) {
+      meaning = "we don't have in our database";
+    }
+
+    setMeaning(meaning);
+  }
+
+  function colorClickHandler(color) {
+    var meaning = colorDictionary[color];
+    setMeaning(meaning);
+  }
+
   return (
     <div className="App">
-      <h1>Flag Identifier</h1>
+      <h1 style={{ textDecoration: "underline" }}>Color Intepretor</h1>
+      <div>
+        <input
+          onChange={inputColorHandler}
+          style={{ padding: "1rem", width: "80%", height: "50px" }}
+        ></input>
+        <h1>{meaning}</h1>
+        <h3 style={{ fontSize: "larger" }}>Color in the database</h3>
+        {colorsWeKnow.map(function (color) {
+          return (
+            <span
+              key={color}
+              onClick={() => colorClickHandler(color)}
+              style={{
+                fontSize: "2rem",
+                padding: "0.5rem 0.5rem",
+                cursor: "pointer"
+              }}
+            >
+              {color}
+            </span>
+          );
+        })}
+      </div>
     </div>
   );
 }
